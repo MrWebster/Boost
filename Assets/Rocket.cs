7a5791884892d87d257a2af12ae9ea4558d0ac40
Andrew Webster <mr.andrewwebster@gmail.com>
2018-04-02 19:01:15 -0600

    Lecture 45

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource thrustSound;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        thrustSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -20,14 +22,17 @@ public class Rocket : MonoBehaviour {
     private void ProcesInput() {
         if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!thrustSound.isPlaying) thrustSound.Play();
+        } else {
+            thrustSound.Stop();
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
             print("no rotate");
-        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+        } else if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
             print("rotate left");
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
+        } else if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(Vector3.back);
             print("rotate right");
         }
