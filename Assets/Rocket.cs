4b2c1d790b85fff575ad4951620f53b1b5f5c35a
Andrew Webster <mr.andrewwebster@gmail.com>
2018-04-02 18:34:47 -0600

    Lecture 42

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -17,7 +19,7 @@ public class Rocket : MonoBehaviour {
 
     private void ProcesInput() {
         if (Input.GetKey(KeyCode.Space)) {
-            print("thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) {
