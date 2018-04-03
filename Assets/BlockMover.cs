d8c568db8bc985c793b693c8af55a232a3d76ffe
Andrew Webster <mr.andrewwebster@gmail.com>
2018-04-02 22:44:53 -0600

    Lecture 59

---
+++ Assets/BlockMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// prevents this script from acting on the same component twice, if it was attached twice.
+[DisallowMultipleComponent]
+
+public class BlockMover : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+    // Transform obsticalTransform;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+	}
+}
